28a6fc02535d8a3db9158ce541dda07537833eb1
eric-the-geric <eric.gericke2@gmail.com>
2024-12-04 21:23:43 +0100

    failed but trying again

--- day4/main.lua
+++ day4/main.lua
@@ -29,6 +29,27 @@ function Get_words(row)
 	end
 	return words
 end
+function Extract_diaganol(board, start_row, start_col)
+	local diag = {}
+	local r, c = start_row, start_col
+	while r < #board and c < #board[r] do
+		diag[#diag+1] = board[r][c]
+		r = r + 1
+		c = c + 1
+	end
+	return diag
+end
+
+function Extract_diaganol2(board, start_row, start_col)
+	local diag = {}
+	local r, c = start_row, start_col
+	while r < #board and c > 0 do
+		diag[#diag+1] = board[r][c]
+		r = r + 1
+		c = c - 1
+	end
+	return diag
+end
 
 function P1()
 	local board = Construct_board()
@@ -46,9 +67,25 @@ function P1()
 	for i, row in ipairs(horizontal_forward) do
 		local words = Get_words(row)
 		for _, w in ipairs(words) do
+			print(w)
 			all_words[#all_words+1] = w
 		end
 	end
+
+	--local horizontal_reverse = {}
+	--for i=#board, 1, -1 do
+	--	local row = {}
+	--	for j=#board[i],1,-1 do
+	--		row[#row+1] = board[i][j]
+	--	end
+	--	horizontal_reverse[#horizontal_reverse+1] = row
+	--end
+	--for i, row in ipairs(horizontal_reverse) do
+	--	local words = Get_words(row)
+	--	for _, w in ipairs(words) do
+	--		all_words[#all_words+1] = w
+	--	end
+	--end
 
 	-- vertical
 	local vertical_forward = {}
@@ -63,38 +100,89 @@ function P1()
 	for i, row in ipairs(vertical_forward) do
 		local words = Get_words(row)
 		for _, w in ipairs(words) do
+			print(w)
 			all_words[#all_words+1] = w
 		end
 	end
+
+	--local vertical_reverse = {}
+	--for i=#board, 1, -1 do
+	--	local row = {}
+	--	for j=#board[i],1,-1 do
+	--		row[#row+1] = board[j][i]
+	--	end
+	--	vertical_reverse[#vertical_reverse+1] = row
+	--end
+
+	--for i, row in ipairs(vertical_reverse) do
+	--	local words = Get_words(row)
+	--	for _, w in ipairs(words) do
+	--		all_words[#all_words+1] = w
+	--	end
+	--end
 
 	-- diagonal
 	local diagonal_forward = {}
 	for i=1,#board do
-		local row = {}
-		for j=1,#board[i] do
-			for k=1,#board-i do
-				for m=1,#board-j do
-					row[#row+1] = board[i+k][j+m]
-				end
-			end
-		end
-		diagonal_forward[#diagonal_forward+1] = row
+		diagonal_forward[#diagonal_forward+1] = Extract_diaganol(board, i, 1)
 	end
-
+	for i=1,#board[1] do
+		diagonal_forward[#diagonal_forward+1] = Extract_diaganol(board, 1, i)
+	end
 	for i, row in ipairs(diagonal_forward) do
 		local words = Get_words(row)
 		for _, w in ipairs(words) do
+			print(w)
 			all_words[#all_words+1] = w
 		end
 	end
 
+	local diagonal_reverse = {}
+	for i=1,#board do
+		diagonal_reverse[#diagonal_reverse+1] = Extract_diaganol2(board, i, #board[1])
+	end
+
+	for i = #board, 1, -1 do
+		diagonal_reverse[#diagonal_reverse+1] = Extract_diaganol2(board, 1, i)
+	end
+
+	for i, row in ipairs(diagonal_reverse) do
+		local words = Get_words(row)
+		for _, w in ipairs(words) do
+			print(w)
+			all_words[#all_words+ 1] = w
+		end
+	end
+
+
+
+
+	--local diagonal_reverse = {}
+	--for i=#board,1,-1 do
+	--	local row = {}
+	--	for j=#board[i],1,-1 do
+	--		for k=#board,1+i,-1 do
+	--			for m=#board,1+j,-1 do
+	--				if k == m then
+	--					row[#row+1] = board[i-k][j-m]
+	--				end
+	--			end
+	--		end
+	--	end
+	--	diagonal_reverse[#diagonal_reverse+1] = row
+	--end
+
+	--for i, row in ipairs(diagonal_reverse) do
+	--	local words = Get_words(row)
+	--	for _, w in ipairs(words) do
+	--		all_words[#all_words+1] = w
+	--	end
+	--end
 	local sum = 0
 	for _, w in ipairs(all_words) do
-		if w == "XMAS" then sum = sum +1 end
+		if w == "XMAS" or w == "SAMX" then sum = sum +1 end
 	end
 	print(sum)
-
-	-- diagonal
 end
 
 
