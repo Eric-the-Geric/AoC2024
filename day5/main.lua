7dcc16dbfc12567bb1d34abab1539b7cfdf62e1c
Eric-the-Geric <eric.gericke2@gmail.com>
2024-12-06 12:28:00 +0100

    p2 completelua main.lua !

--- day5/main.lua
+++ day5/main.lua
@@ -2,7 +2,7 @@ File = './input.txt'
 io.input(File)
 
 
-function Read_input()
+local function Read_input()
 	local rules = {}
 	local prints = {}
 	local printing = false
@@ -32,43 +32,99 @@ function Read_input()
 	end
 	return prints, rules
 end
-function P1()
-	local prints, rules = Read_input()
-	-- loop through the prints
+
+
+local function Check(report, rules)
+	local check = true
+	for i, initial_v in ipairs(report) do
+		initial_v = tonumber(initial_v)
+		-- loop through ruleset and find a rule that has v in it
+		for _, rule in ipairs(rules) do
+			local before = rule[1]
+			local after = rule[2]
+			if initial_v == before then
+				for index, v in ipairs(report) do
+					v = tonumber(v)
+					if v == after then
+						if index < i then check = false break end
+					end
+				end
+			end
+		end
+	end
+	return check
+end
+local function Check_prints(prints, rules, correct)
 	local clean_prints = {}
 	for _, report in ipairs(prints) do
 		-- initial loop through each value in print
 		-- create a check to see if the print is valid
-		local check = true
-		for i, initial_v in ipairs(report) do
-			initial_v = tonumber(initial_v)
-			-- loop through ruleset and find a rule that has v in it
-			for _, rule in ipairs(rules) do
-				local before = rule[1]
-				local after = rule[2]
-				if initial_v == before then
-					for index, v in ipairs(report) do
-						v = tonumber(v)
-						if v == after then
-							if index < i then check = false break end
+		local check = Check(report, rules)
+		if correct then
+			if check == true then clean_prints[#clean_prints+1] = report end
+		else
+			if check == false then clean_prints[#clean_prints+1] = report end
+		end
+	end
+	return clean_prints
+end
+
+local function P1(prints, rules)
+	--local prints, rules = Read_input()
+	-- loop through the prints
+	local clean_prints = Check_prints(prints, rules, true)
+	local sum = 0
+	for _, rep in ipairs(clean_prints) do
+		--print(table.concat(rep, ",", 1, #rep))
+		local middle_num = tonumber(rep[(#rep+1)/2])
+		sum = sum + middle_num
+	end
+	return sum
+end
+
+local function Sort_prints(clean_prints, rules)
+	local sorted_prints = {}
+	for _, report in ipairs(clean_prints) do
+		while not Check(report, rules) do
+			for i, initial_v in ipairs(report) do
+				initial_v = tonumber(initial_v)
+				-- loop through ruleset and find a rule that has v in it
+				for _, rule in ipairs(rules) do
+					local before = rule[1]
+					local after = rule[2]
+					if initial_v == before then
+						for index, v in ipairs(report) do
+							v = tonumber(v)
+							if v == after then
+								if index < i then report[index], report[i] = report[i], report[index]; break end
+							end
 						end
 					end
 				end
 			end
 		end
-		if check == true then clean_prints[#clean_prints+1] = report end
-		--break
+		sorted_prints[#sorted_prints+1] = report
 	end
+	return sorted_prints
+end
+
+local function P2(prints, rules)
+	--local prints, rules = Read_input()
+	local clean_prints = Check_prints(prints, rules, false)
+	local sorted_prints = Sort_prints(clean_prints, rules)
+
 	local sum = 0
-	for _, rep in ipairs(clean_prints) do
-		print(table.concat(rep, ",", 1, #rep))
+	for _, rep in ipairs(sorted_prints) do
+		--print(table.concat(rep, ",", 1, #rep))
 		local middle_num = tonumber(rep[(#rep+1)/2])
 		sum = sum + middle_num
 	end
-	print(sum)
+	return sum
 end
+local prints, rules = Read_input()
+local p1_sum = P1(prints, rules)
+local p2_sum = P2(prints, rules)
 
-function P2()
-end
+print("p1", p1_sum)
+print("p2", p2_sum)
 
-P1()
