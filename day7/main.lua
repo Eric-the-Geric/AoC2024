57fd3841cf64101495e120e0cf778ecafe941808
eric-the-geric <eric.gericke2@gmail.com>
2024-12-08 20:05:24 +0100

    completed day 7

--- day7/main.lua
+++ day7/main.lua
@@ -1,4 +1,4 @@
-File = './example.txt'
+File = './input.txt'
 io.input(File)
 
 
@@ -15,57 +15,62 @@ function read_input()
 end
 
 function find_all_combinations(n_ops, n_combs)
-	local index = 1
-	local previous_comb = {}
-	for i=1,n_combs do
-		previous_comb[#previous_comb+1] = 0
-	end
-	local all_combs = {previous_comb}
-	local offset = 1
-	while #all_combs < n_ops^n_combs do
-		local new_comb = {}
-		for i, num in ipairs(previous_comb) do
-			new_comb[#new_comb+1] = num
-		end
-		new_comb[index] = (new_comb[index]%n_ops + 1) %n_ops
-		index = ((index + offset%index) % n_combs) + 1
-		previous_comb = new_comb
-		offset = offset + offset%n_combs
-		all_combs[#all_combs+1] = previous_comb
-	end
-	print(#all_combs)
-	for _, row in ipairs(all_combs) do
-		print(table.unpack(row, 1,#row))
-	end
+    local all_combinations = {}
+    local total_combinations = n_ops^n_combs
 
-
+    for i = 0, total_combinations - 1 do
+        local combination = {}
+        for j = 1, n_combs do
+            table.insert(combination, math.floor(i / (n_ops^(j-1))) % n_ops)
+        end
+        table.insert(all_combinations, combination)
+    end
+	return all_combinations
 end
+
 function p1()
 	local inputs = read_input()
-	local operators = {"0", "1"}
-	local num_operators = 0
+	local true_answer = 0
 	for _, line in ipairs(inputs) do
-		num_operators = #line -2
-		local operator_comb = {}
-		for i=1, num_operators do
-			local comb = {}
-			for j=1, num_operators do
-				comb[#comb+1] = operators[(i + j)%2 +1]
-			end
-			--print(#comb)
-			--print(table.unpack(comb, 1, #comb))
-			operator_comb[#operator_comb+1] = comb
-		end
-		print(#operator_comb)
 		local answer = line[1]
-		local eval = 0
-		for i=2,#line do
+		local n_ops = 2
+		local n_combs = #line-2
+		local combs = find_all_combinations(n_ops, n_combs)
+		for tmp, ops in ipairs(combs) do
+			local sum = line[2]
+			for i=3, #line do
+				if ops[i-2] == 0 then sum = sum + line[i]
+				else
+					sum = sum * line[i]
+				end
+			end
+			if sum == answer then true_answer = true_answer + sum; break end
 		end
-
 	end
+	print(true_answer)
 end
 
 function p2()
-end
+	local inputs = read_input()
+	local true_answer = 0
+	for _, line in ipairs(inputs) do
+		local answer = line[1]
+		local n_ops = 3
+		local n_combs = #line-2
+		local combs = find_all_combinations(n_ops, n_combs)
+		for tmp, ops in ipairs(combs) do
+			local sum = line[2]
+			for i=3, #line do
+				if ops[i-2] == 0 then sum = sum + line[i]
+				elseif ops[i-2] == 1 then sum = sum * line[i]
+				else
+					sum = tonumber(tostring(sum) .. tostring(line[i]))
 
-find_all_combinations(2, 3)
+				end
+			end
+			if sum == answer then true_answer = true_answer + sum; break end
+		end
+	end
+	print(true_answer)
+end
+p2()
