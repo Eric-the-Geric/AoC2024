de0d72b0e4b1716920eb7f2db8fa31bf2b8f508d
eric-the-geric <eric.gericke2@gmail.com>
2024-12-11 10:29:26 +0100

    added p2 day10. only 1 line of code needed changing haha

--- day10/main.lua
+++ day10/main.lua
@@ -45,6 +45,46 @@ function make_covermap(map)
 	end
 	return covered_map
 end
+
+function find_neighbours2(map, x, y, tally, covered_map)
+	if map[x][y] == 9 then
+		--covered_map[x][y] = '#'
+		tally = tally + 1
+		return tally
+	end
+	local current = map[x][y]
+	covered_map[x][y] = "#"
+	for i=-1, 1 do
+		for j=-1, 1 do
+			if math.abs(i) ~= math.abs(j) and x+i <= #map and x+i > 0 and y+j <= #map[x] and y+j > 0 then
+				local neighbour = map[x+i][y+j]
+				if neighbour - current == 1 then
+					tally = find_neighbours2(map, x+i, y+j, tally, covered_map)
+				end
+			end
+		end
+	end
+	return tally
+end
+function p2()
+	local map = read_input()
+	local trail_heads = {}
+	for i, row in ipairs(map) do
+		for j, val in ipairs(row) do
+			if val == 0 then trail_heads[#trail_heads+1] = {i, j} end
+		end
+	end
+	local score = 0
+	for i, xy in ipairs(trail_heads) do
+		local covered_map = make_covermap(map)
+		--score = score + 
+		local tally = find_neighbours2(map, xy[1], xy[2], 0, covered_map)
+		print(tally)
+		score = score + tally
+	end
+	print(score)
+end
+
 function p1()
 	local map = read_input()
 	local trail_heads = {}
@@ -53,16 +93,14 @@ function p1()
 			if val == 0 then trail_heads[#trail_heads+1] = {i, j} end
 		end
 	end
-	
 	local score = 0
 	for i, xy in ipairs(trail_heads) do
 		local covered_map = make_covermap(map)
 		--score = score + 
 		local tally = find_neighbours(map, xy[1], xy[2], 0, covered_map)
-		print(tally)
 		score = score + tally
 	end
 	print(score)
 end
 
-p1()
+p2()
