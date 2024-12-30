213e13f0e4997d5b3f683a7076885fc5f0978482
Eric-the-Geric <eric.gericke2@gmail.com>
2024-12-30 16:57:15 +0100

    added day19 but not completed, just parsing :P

---
+++ day19/main.py
@@ -0,0 +1,22 @@
+FILE = "./example.txt"
+def p1(avail_pats, designs):
+    for design in designs:
+        s
+        while True:
+
+
+
+def main():
+    with open(FILE, 'r') as f:
+        avail_pats, designs = f.read().split("\n\n")
+
+    avail_pats = avail_pats.strip().split(",")
+    designs = designs.strip().split("\n")
+    
+
+
+
+
+main()
+
+
